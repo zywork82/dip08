eff0f7c72b789c3d53eeed9d3e7e354d208fb895
chloechuajy <chloeechuajy@gmail.com>
2025-10-15 15:32:01 +0800

    trainerteam and casestudies

--- frontend/src/pages/ScenarioPrompt.jsx
+++ frontend/src/pages/ScenarioPrompt.jsx
@@ -8,7 +8,7 @@ import ScenarioHistory from "../components/ScenarioHistory";
 
 
 // Placeholder for the profile image
-const profileImage = 'https://placehold.co/40x40/E6E6FA/3f51b5?text=Prof+A';
+const profileImage = 'https://i.pinimg.com/1200x/9e/83/75/9e837528f01cf3f42119c5aeeed1b336.jpg';
 const ScenarioPrompt = () => {
   const navigate = useNavigate();
   const [title, setTitle] = useState("");
